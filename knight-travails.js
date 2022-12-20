7ab90e37381a9178810581428c867c84ed63d40b
Carl Madsen <madsencarlb@gmail.com>
2022-12-20 23:28:28 +1100

    finish knifeMoves function

--- knight-travails.js
+++ knight-travails.js
@@ -1,33 +1,45 @@
-function knightMoves(knight, dest) {
+function knightMoves(knightLoc, dest) {
     let gameBoard = createBoard(8, 8);
-    if (!checkValidSquare(knight) || !checkValidSquare(dest)) {
+    if (!checkValidSquare(gameBoard, knightLoc) || !checkValidSquare(gameBoard, dest)) {
         return "Please input a valid knight location and destination";
     }
-    let [destX, destY] = dest;
-
-    console.log(knightMovesList)
+    let queue = [createKnightObject(knightLoc)];
+    let moveTotal = 0;
+    // Recursive function to find the list of knight moves to reach the destination
+    // Returns an object with loc = location and prev = object representing previous lcoation
+    function moveSearch(index = 0) {
+        if (!queue[index]) return;
+        if (index > 1000000) return;
+        let current = queue[index];
+        if (current.loc[0] === dest[0] && current.loc[1] === dest[1]) return current;
+        let nextMoves = getKnightMoves(gameBoard, current.loc);
+        nextMoves.forEach(obj => {
+            let possibleMove = createKnightObject(obj);
+            possibleMove.prev = current;
+            queue.push(possibleMove);
+        });
+        moveTotal++
+        return moveSearch(index + 1);
+    }
+    // Get the knight object with nested prev properties and reformat into an array of moves
+    let moveObject = moveSearch();
+    let moveArray = [];
+    while (moveObject){
+        moveArray.unshift(moveObject.loc);
+        moveObject = moveObject.prev;
+    }
+    console.log(`=> You made it in ${moveArray.length - 1} moves! Here's your path:`)
+    for (let i = 0; i < moveArray.length; i++){
+        console.log(`[${moveArray[i]}]`)
+    }
 }
 
-// Recursive function to find the list of knight moves to reach the destination
-function knightRec(knight, dest) {
-    if (knight === dest) return dest;
-    let knightMovesList = getKnightMoves(gameBoard, knight)
+function createKnightObject(loc) {
+    return {
+        loc: loc,
+        prev: null
+    };
 }
-
-
-// levelOrder(fn, root = this.root) {
-//     let arr = [root];
-//     function pushLevelOrder(index = 0) {
-//         if (!arr[index]) return;
-//         let current = arr[index];
-//         if (current.left) arr.push(current.left);
-//         if (current.right) arr.push(current.right);
-//         pushLevelOrder(index + 1);
-//     }
-//     pushLevelOrder();
-//     return this.mapNodes(arr, fn);
-// }
-
 
 // Create an array-based grid of size x, y
 function createBoard(x, y) {
@@ -43,26 +55,26 @@ function createBoard(x, y) {
 }
 
 // Return a list of possible squares for the knight to move to
-function getKnightMoves(gameBoard, knight) {
+// This nested loop is less efficient than just creating 8 different moves, but less verbose (and a bit more fun)
+function getKnightMoves(gameBoard, knightLoc) {
     let movesArray = [];
     for (let y = -2; y <= 2; y++) {
         for (let x = -2; x <= 2; x++) {
             // Knight moves a total of 3 orthogonal squares, so we can easily trim for valid moves
             if (Math.abs(y) + Math.abs(x) === 3) {
-                let knightX = knight[1] + x;
-                let knightY = knight[0] + y;
+                let knightX = knightLoc[0] + x;
+                let knightY = knightLoc[1] + y;
                 if (checkValidSquare(gameBoard, [knightX, knightY])) {
                     movesArray.push([knightX, knightY]);
                 }
             }
         }
     }
-    return movesArray
+    return movesArray;
 }
 
 // Check if the given location is a valid square on the board
 function checkValidSquare(gameBoard, move) {
-    console.log(move)
     if (gameBoard[move[1]] !== undefined &&
         gameBoard[move[1]][move[0]] !== undefined) {
         return true;
@@ -70,4 +82,6 @@ function checkValidSquare(gameBoard, move) {
     else return false;
 }
 
-knightMoves([0, 0], [3, 3])
+knightMoves([0, 0], [7, 7])
+knightMoves([0, 1], [7, 7])
+knightMoves([4, 2], [7, 7])
